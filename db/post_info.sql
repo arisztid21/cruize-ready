7c678fc3279f3a985c52f2ccff75adecf7cc3d7d
arisztid munoz <arisztidmunoz@arisztids-MacBook-Pro.local>
2018-10-24 17:57:41 -0700

    fixed listing render bug

--- db/post_info.sql
+++ db/post_info.sql
@@ -1,4 +1,2 @@
--- select * from posts where id = $1;
-
 SELECT users.username, posts.* FROM users JOIN
-posts ON(users.id = posts.seller_id) where users.id = $1;
+posts ON(users.id = posts.seller_id) where posts.id = $1;
